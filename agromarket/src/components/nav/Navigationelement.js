9800c8f71b61ec1fd83e10b65a6bc9d4fcf917d4
leotalorac <leotalorac@unal.edu.co>
2019-10-26 09:50:57 -0500

    finish change icon

--- agromarket/src/components/nav/Navigationelement.js
+++ agromarket/src/components/nav/Navigationelement.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingBasket,faMedal,faMoneyBillWave,faExchangeAlt} from '@fortawesome/free-solid-svg-icons'
+import { faShoppingBasket,faMedal,faStream,faExchangeAlt} from '@fortawesome/free-solid-svg-icons'
 
 
-let icons = [faShoppingBasket, faMedal,faExchangeAlt,faMoneyBillWave]
+let icons = [faShoppingBasket, faMedal,faExchangeAlt,faStream]
 export default function Navigationelement(props) {
     
     return (
